553f4da39006e0056b260723b866a1338fcf09f3
Anastasia343 <bol.anastasia3745@gmail.com>
2023-02-17 15:15:28 +0500

    зн

--- WindowsFormsApp111/Form1.cs
+++ WindowsFormsApp111/Form1.cs
@@ -16,5 +16,24 @@ namespace WindowsFormsApp111
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = int.Parse(textBox1.Text);
+            b = 1;
+            c = 0;
+            for(int i = a; i<a+9999; i++)
+            {
+                for(b =1; b <= 12; b++)
+                {
+                    DateTime qwe = new DateTime(i, b, 13);
+                    if (qwe.DayOfWeek == DayOfWeek.Friday)
+                        c += 1;
+                }
+            }
+            MessageBox.Show($"{c}");
+
+        }
     }
 }
